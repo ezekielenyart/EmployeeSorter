b82ee5126c3c9ef666da2008ac46314e13273309
Ezekiel <zekejenyart@gmail.com>
2020-08-12 16:56:12 -0500

    Updates

---
+++ precache-manifest.131136f48c570e59e326f3b88c376e63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "232d12bc86eeb68659a3248c3df71386",
+    "url": "/EmployeeSorter/index.html"
+  },
+  {
+    "revision": "fee97857a791dcb90a35",
+    "url": "/EmployeeSorter/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3e403d7db7a595f95ee8",
+    "url": "/EmployeeSorter/static/js/2.d61cb1ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/EmployeeSorter/static/js/2.d61cb1ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee97857a791dcb90a35",
+    "url": "/EmployeeSorter/static/js/main.c0d5dd84.chunk.js"
+  },
+  {
+    "revision": "6a773f130d7c1265e98e",
+    "url": "/EmployeeSorter/static/js/runtime-main.357360c9.js"
+  }
+]);
